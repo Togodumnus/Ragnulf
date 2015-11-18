72cd1a5c46409772e50dc35df223491173b12d15
Guillaume CLOCHARD <guillaume.clochard@etu.univ-nantes.fr>
2015-11-18 20:11:25 +0100

    Init class Cube
    Init de la class Cube. Ajout des fonctions des rotations vides.

---
+++ Cube.py
@@ -0,0 +1,112 @@
+
+class Cube():
+
+    def __init__(self, str):
+
+    def rot_L():
+        """
+        rot_L
+
+        Rotation de la face gauche (Left)
+        """
+        #TODO
+        pass
+
+    def rot_Li():
+        """
+        rot_Li
+
+        Rotation inverse de la face gauche (Left)
+        """
+        #TODO
+        pass
+
+    def rot_R():
+        """
+        rot_R
+
+        Rotation de la face droite (Right)
+        """
+        #TODO
+        pass
+
+    def rot_Ri():
+        """
+        rot_Ri
+
+        Rotation inverse de la face droite (Right)
+        """
+        #TODO
+        pass
+
+    def rot_F():
+        """
+        rot_F
+
+        Rotation de la face avant (Front)
+        """
+        #TODO
+        pass
+
+    def rot_Fi():
+        """
+        rot_Fi
+
+        Rotation inverse de la face avant (Front)
+        """
+        #TODO
+        pass
+
+    def rot_B():
+        """
+        rot_B
+
+        Rotation de la face arrière (Back)
+        """
+        #TODO
+        pass
+
+    def rot_Bi():
+        """
+        rot_Bi
+
+        Rotation inverse de la face arrière (Back)
+        """
+        #TODO
+        pass
+
+    def rot_U():
+        """
+        rot_U
+
+        Rotation de la face du haut (Up)
+        """
+        #TODO
+        pass
+
+    def rot_Li():
+        """
+        rot_Ui
+
+        Rotation inverse de la face du haut (Up)
+        """
+        #TODO
+        pass
+
+    def rot_D():
+        """
+        rot_D
+
+        Rotation de la face du bas (Down)
+        """
+        #TODO
+        pass
+
+    def rot_Di():
+        """
+        rot_Di
+
+        Rotation inverse de la face du bas (Down)
+        """
+        #TODO
+        pass
