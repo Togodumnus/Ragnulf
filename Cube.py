6137116f336691497c1eb9819c4866c6170eb3ad
Guillaume CLOCHARD <guillaume.clochard@etu.univ-nantes.fr>
2015-11-18 21:59:07 +0100

    Ajout commentaire

--- Cube.py
+++ Cube.py
@@ -15,12 +15,29 @@ def Array(arr):
     return np.array(arr, dtype=np.int8)
 
 class Cube():
+    """
+    Cube
+
+    La structure de donnée modélisant un cube ainsi que les fonctions de
+    rotations qui s'y appliquent
+
+    @see https://gitlab.univ-nantes.fr/E132397K/Ragnulf/issues/4 pour
+    l'histoirique des choix effectués
+    """
 
     def __init__(self):
         """
         __init__
 
         Création d'une nouvelle instance de Cube
+
+        Codage des couleurs :
+            White  (W) = 0
+            Blue   (B) = 1
+            Red    (R) = 2
+            Green  (G) = 3
+            Orange (0) = 4
+            Yellow (Y) = 5
         """
 
         self.cubes = {
