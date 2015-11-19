5af0268b572753cdec2107d2d42144061c5d4a11
Jimmy DORE <jimmy.dore@etu.univ-nantes.fr>
2015-11-19 19:37:59 +0100

    Fonction fonctionelle - prend une chaine de caractères et renvoie l'objet cube initialisé avec la chaine

--- lire_entree.py
+++ lire_entree.py
@@ -1,4 +1,5 @@
 from Cube import Cube
+from utils import Array,colorToCode
 # Analyse de l'entrée pour modéliser le cube par la suite
 # Entrée sous la forme : 
 # cube = 'OGRBWYBGBGYYOYOWOWGRYOOOBGBRRYRBWWWRBWYGROWGRYBRGYWBOG'
@@ -43,8 +44,10 @@ def faces(str):
 
 
 
-'''
-# Str de 48 caractères à transformer en 21 cubes de 2 ou 3 caractères. 
+
+def data_cube_dictionnaire(str_cube):
+    '''
+    data_cube_dictionnaire 
 
  
  Ne pas oublier
@@ -59,13 +62,10 @@ def faces(str):
 
 
 
-'''
-def data_cube_dictionnaire(str_cube):
+    '''
     c = Cube()
     facesCube = faces(str_cube)
     #On a les 6 faces mais on ne sait pas quelle face est up, down ..
-
-    print(facesCube)
     
     str_left = None  # Orange 4
     str_front = None # Blue   1
@@ -81,15 +81,41 @@ def data_cube_dictionnaire(str_cube):
             str_front = face
         elif face[4] == 'R':
             str_right = face
-        elif face[4] == 'G'
+        elif face[4] == 'G':
             str_back = face
-        elif face[4] = 'W'
+        elif face[4] == 'W':
             str_down = face
-        elif face[4] == 'Y'
+        elif face[4] == 'Y':
             str_up = face
         else :
             return None
-    
+
+
+    c.cubes['FU'] = Array([colorToCode(str_front[1]),colorToCode(str_up[7])])
+    c.cubes['FRU'] = Array([colorToCode(str_front[2]),colorToCode(str_right[0]),colorToCode(str_up[8])])
+    c.cubes['FR'] = Array([colorToCode(str_front[5]),colorToCode(str_right[3])])
+    c.cubes['FRD'] = Array([colorToCode(str_front[8]),colorToCode(str_right[6]),colorToCode(str_down[2])])
+    c.cubes['FD'] = Array([colorToCode(str_front[7]),colorToCode(str_down[1])])
+    c.cubes['FLD'] = Array([colorToCode(str_front[6]),colorToCode(str_left[8]),colorToCode(str_down[0])])
+    c.cubes['FL'] = Array([colorToCode(str_front[3]),colorToCode(str_left[5])])
+    c.cubes['FLU'] = Array([colorToCode(str_front[0]),colorToCode(str_left[2]),colorToCode(str_up[6])])
+
+    c.cubes['LU'] = Array([colorToCode(str_left[1]),colorToCode(str_up[3])])
+    c.cubes['LD'] = Array([colorToCode(str_left[7]),colorToCode(str_down[3])])
+
+    c.cubes['BU'] = Array([colorToCode(str_back[1]),colorToCode(str_up[1])])
+    c.cubes['BRU'] = Array([colorToCode(str_back[0]),colorToCode(str_right[2]),colorToCode(str_up[2])])
+    c.cubes['BR'] = Array([colorToCode(str_back[3]),colorToCode(str_right[5])])
+    c.cubes['BRD'] = Array([colorToCode(str_back[6]),colorToCode(str_right[8]),colorToCode(str_down[8])])
+    c.cubes['BD'] = Array([colorToCode(str_back[7]),colorToCode(str_down[7])])
+    c.cubes['BLD'] = Array([colorToCode(str_back[8]),colorToCode(str_left[6]),colorToCode(str_down[6])])
+    c.cubes['BL'] = Array([colorToCode(str_back[5]),colorToCode(str_left[3])])
+    c.cubes['BLU'] = Array([colorToCode(str_back[2]),colorToCode(str_left[0]),colorToCode(str_up[0])])
+
+    c.cubes['RU'] = Array([colorToCode(str_right[1]),colorToCode(str_up[5])])
+    c.cubes['RD'] = Array([colorToCode(str_right[7]),colorToCode(str_down[5])])
+
+    return c
 
 
 
@@ -100,3 +126,4 @@ if __name__ == "__main__":
 
     str_cube = "OGRBWYBGBGYYOYOWOWGRYOOOBGBRRYRBWWWRBWYGROWGRYBRGYWBOG"
     cube = data_cube_dictionnaire(str_cube)
+    print(cube)
