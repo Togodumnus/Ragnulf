8c4eedc7db14ccb060f885df8e45a62a63b3370c
Jimmy DORE <jimmy.dore@etu.univ-nantes.fr>
2015-11-19 16:29:22 +0100

    test main fonction pas encore faite

--- lire_entree.py
+++ lire_entree.py
@@ -8,3 +8,9 @@ def data_cube_dictionnaire(str_cube):
 	#TODO On prend une chaîne de caractère en entrée, mais qu'est ce que l'on renvoie ?
 	# un cube initialisé ? Voir avec Guillaume
 
+
+
+if __name__ == "__main__":
+
+	str_cube = "OGRBWYBGBGYYOYOWOWGRYOOOBGBRRYRBWWWRBWYGROWGRYBRGYWBOG"
+	cube = data_cube_dictionnaire(str_cube)
