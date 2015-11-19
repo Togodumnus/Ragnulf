3fb0fcfa2f8ba49ff6b572cda7209f2a00c1b83f
Jimmy DORE <jimmy.dore@etu.univ-nantes.fr>
2015-11-19 16:10:28 +0100

    Ajout de commentaires pour compréhension lecture entree

---
+++ lire_entree.py
@@ -0,0 +1,10 @@
+# Analyse de l'entrée pour modéliser le cube par la suite
+# Entrée sous la forme : 
+# cube = 'OGRBWYBGBGYYOYOWOWGRYOOOBGBRRYRBWWWRBWYGROWGRYBRGYWBOG'
+
+import numpy as np
+
+def data_cube_dictionnaire(str_cube):
+	#TODO On prend une chaîne de caractère en entrée, mais qu'est ce que l'on renvoie ?
+	# un cube initialisé ? Voir avec Guillaume
+
