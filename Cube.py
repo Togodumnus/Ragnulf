0a270803e08647e1210f4fdfd5aab0f4ec576ff0
Guillaume CLOCHARD <guillaume.clochard@etu.univ-nantes.fr>
2015-11-19 09:13:16 +0100

    [WIP] Affichage du cube

--- Cube.py
+++ Cube.py
@@ -1,4 +1,4 @@
-from utils import Array
+from utils import Array, codeToColor
 
 class Cube():
     """
@@ -64,6 +64,64 @@ class Cube():
             'RD'  : Array([]),
 
         }
+
+    def __str__(self):
+        """
+        On veut retourner une chaîne du genre:
+                   O G R
+                   B W Y
+                   B G B
+            G Y Y  O Y O  W O W  G R Y
+            O O O  B G B  R R Y  R B W
+            W W R  B W Y  G R O  W G R
+                   Y B R
+                   G Y W
+                   B O G
+        """
+
+        #Un carré d'espaces pour les blocs vides du patron ci-dessus
+        empty = [
+            [' ', ' ', ' '],
+            [' ', ' ', ' '],
+            [' ', ' ', ' ']
+        ]
+
+        up = [
+            [self.cubes['BLU'][2], self.cubes['BU'][1], self.cubes['BRU'][2]],
+            [self.cubes['LU'][1],  5,                   self.cubes['LR'][1]],
+            [self.cubes['FLU'][2], self.cubes['FU'][1], self.cubes['FRU'][2]],
+        ]
+
+        left = [
+            [self.cubes['BLU'][1], self.cubes['LU'][0], self.cubes['FLU'][1]],
+            [self.cubes['BL'][1],  4,                   self.cubes['FL'][1]],
+            [self.cubes['BLD'][1], self.cubes['LD'][0], self.cubes['FLD'][1]],
+        ]
+
+        front = [
+            [self.cubes['FLU'][0], self.cubes['FU'][0], self.cubes['FRU'][0]],
+            [self.cubes['FL'][0],  1,                   self.cubes['FR'][0]],
+            [self.cubes['FLD'][0], self.cubes['FD'][0], self.cubes['FRD'][0]],
+        ]
+
+        right = [
+            [self.cubes['FRU'][1], self.cubes['RU'][0], self.cubes['BRU'][1]],
+            [self.cubes['FR'][1],  2,                   self.cubes['BR'][1]],
+            [self.cubes['FRD'][1], self.cubes['RD'][0], self.cubes['BRD'][1]],
+        ]
+
+        back = [
+            [self.cubes['BRU'][0], self.cubes['BU'][0], self.cubes['BLU'][0]],
+            [self.cubes['BR'][0],  3,                   self.cubes['BL'][0]],
+            [self.cubes['BRD'][0], self.cubes['BD'][0], self.cubes['BLD'][0]],
+        ]
+
+        down = [
+            [self.cubes['FLD'][2], self.cubes['FD'][1], self.cubes['FRD'][2]],
+            [self.cubes['LD'][1],  0,                   self.cubes['RD'][1]],
+            [self.cubes['BLD'][2], self.cubes['BD'][1], self.cubes['BRD'][2]],
+        ]
+
 
 
     def rot_L():
