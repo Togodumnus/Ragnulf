6170c3ffc28ec86466300d52f114caea63d3df07
Arnaud LANGELIER <arnaud.langelier@etu.univ-nantes.fr>
2015-11-27 12:42:10 +0100

    Creation d'un fichier Cube_facettes.py dont la structure sera la liste des facettes, pour test de rapidité

---
+++ Cube_facettes.py
@@ -0,0 +1,30 @@
+from utils import Array, codeToColor
+from numpy import copy as np_copy
+
+class Cube():
+    """
+    Cube
+
+    La structure de donnée modélisant un cube ainsi que les fonctions de
+    rotations qui s'y appliquent
+
+    @see https://gitlab.univ-nantes.fr/E132397K/Ragnulf/issues/4 pour
+    l'histoirique des choix effectués
+
+    Codage des couleurs :
+        White  (W) = 0
+        Blue   (B) = 1
+        Red    (R) = 2
+        Green  (G) = 3
+        Orange (0) = 4
+        Yellow (Y) = 5
+
+    Convention des couleurs des faces :
+        Down  - White
+        Front - Blue
+        Right - Red
+        Back  - Green
+        Left  - Orange
+        Up    - Yellow
+    """
+
