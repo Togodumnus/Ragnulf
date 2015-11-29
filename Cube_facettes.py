dae04ee89be914fab67bd3ab812d61a79aa5a399
Guillaume CLOCHARD <guillaume.clochard@etu.univ-nantes.fr>
2015-11-29 19:52:13 +0100

    Utilisation des couleurs pour l'affichage du cube

--- Cube_facettes.py
+++ Cube_facettes.py
@@ -1,4 +1,4 @@
-from utils import Array, codeToColor
+from utils import Array, codeToColor, colorize
 from numpy import copy as np_copy
 
 class Cube():
@@ -64,7 +64,7 @@ class Cube():
         space = [' ']
 
         #Une lignes d'espaces pour les blocs vides du patron ci-dessus
-        empty = space * 3
+        empty = space * 9
 
         up = [
             [self.cubes[0],  self.cubes[1],  self.cubes[2]],
@@ -107,7 +107,7 @@ class Cube():
             for ligne in range(3):
                 for c in range(3):
                     #pour chaque case de chaque ligne de chaque face
-                    face[ligne][c] = codeToColor(face[ligne][c])
+                    face[ligne][c] = colorize(codeToColor(face[ligne][c]))
 
         result = [] #tableau de toutes les lignes à afficher
 
