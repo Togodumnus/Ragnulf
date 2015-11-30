4c3455ef2f727566b9b8dcae7987666d6ec39c5b
Guillaume CLOCHARD <guillaume.clochard@etu.univ-nantes.fr>
2015-11-30 18:45:12 +0100

    Test temps d'exécution modélisation petits cubes

---
+++ testCube.py
@@ -0,0 +1,25 @@
+from random import shuffle
+from Cube import Cube
+
+rotations = ['U', 'Ui', 'D', 'Di', 'B', 'Bi', 'F', 'Fi', 'L', 'Li', 'R', 'Ri']
+
+def test():
+    """
+    test
+
+    Test du temps d'exécution des rotations pour la modélisation du Cube
+    par ces petits cubes
+
+    :Example:
+        $ time python testCube.py
+    """
+
+    c = Cube()
+    shuffle(rotations) #on mélange la liste
+    for r in rotations * 12: #on la duplique 12 fois
+        rotation = getattr(c, 'rot_' + r) #on va chercher la fonction de rotation
+        rotation()
+
+if __name__ == '__main__':
+    test()
+
