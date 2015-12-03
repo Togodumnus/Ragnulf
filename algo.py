3ebdeeb723c7162565b7ce791e96214966166d1c
Jimmy DORE <jimmy.dore@etu.univ-nantes.fr>
2015-12-03 15:24:19 +0100

    mini description de l'algo

---
+++ algo.py
@@ -0,0 +1,23 @@
+'''
+Algo de résolution 
+
+
+Pour cet algo, on va utiliser la méthode CFOP décrite dans cette vidéo : 
+https://www.youtube.com/watch?v=VwvGWNfcgs8
+
+Fridrich method [CFOP]
+
+--> Cross
+http://www.youtube.com/watch?v=WzE7SyDB8vA
+
+--> First Two Layers
+http://www.youtube.com/watch?v=IT5BPHEZGJE
+
+
+--> Orienting Last Layer
+http://www.youtube.com/watch?v=q5ltbjGIosU
+
+--> Permuting last Layer
+
+
+'''
