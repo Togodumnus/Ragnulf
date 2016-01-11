0a241718955bd625303123cc2d9acc50b9dfb658
Guillaume CLOCHARD <guillaume.clochard@etu.univ-nantes.fr>
2016-01-11 06:35:20 +0100

    Test algo_cfop() : tests tous les cube
    Et afficher les cubes qui posent problème

--- tests/test_algo.py
+++ tests/test_algo.py
@@ -27,12 +27,23 @@ class TestAlgo(unittest.TestCase):
         with open(JEU_TEST) as data_file: #on parse le jeu de test JSON
             data = json.load(data_file)
             cubes = extract_cubes(data) #1140 cubes
+
+            error = False
+            bugs = []
+
             for cube in cubes:
                 err, c = lecture_cube(cube)
                 if err:
                     raise Error("Erreur lecture_cube \n" + cube)
                 err = algo_cfop(c)[0]
-                self.assertIsNone(err)
+                if err:
+                    bugs.append(cube)
+                    error = True
+
+            self.assertFalse(error, "Certains cubes ne sont pas résolus")
+            if error:
+                for b in bugs:
+                    print(b, "\n")
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
