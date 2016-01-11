970ebe6212f9021d8533577cdb37e65b1844b554
Jimmy DORE <jimmy.dore@etu.univ-nantes.fr>
2016-01-11 19:22:25 +0100

    Ajout d'une fonction split permettant d'afficher le patron du cube et l'image de la rotation cote a cote

--- images_ascii.py
+++ images_ascii.py
@@ -1,3 +1,25 @@
+def splitCubeImg(cube,imgRot):
+    cubeSplit = cube.split("\n")
+    imgSplit = imgRot.split("\n")
+    sizeCube = len(cubeSplit)
+    sizeImg = len(imgSplit)
+
+    strImg = ""
+
+    for i in range(0,max(sizeCube,sizeImg)):
+        if i > sizeCube and i > sizeImg:
+            pass
+        elif i > sizeCube:
+            strImg += imgSplit[i] + "\n"
+        elif i > sizeImg:
+            strImg += cubeSplit[i] + "\n")
+        else:
+            strImg += imgSplit[i]
+            strImg += cubeSplit[i] + "\n"
+
+    return strImg
+
+
 def img_B():
     imgStr = ""
     imgStr+=("                              __________\n");
