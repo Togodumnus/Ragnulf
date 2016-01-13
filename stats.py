fc5562b1fa7e80729d33ec1afce0c61bf2dd0232
Quentin Billaud <quentin.billaud@etu.univ-nantes.fr>
2016-01-13 13:45:32 +0100

    Ajout de la variance dans stats.py

--- stats.py
+++ stats.py
@@ -4,3 +4,10 @@ def moyenne (liste):
 		moyenne += i
 	return moyenne/len(liste)
 
+def variance(liste):
+	variance = 0
+	moy = moyenne(liste)
+	for i in liste:
+		variance += (i-moy)*(i-moy)
+	return variance/len(liste)
+
